4ff8cfa42c07208b548799f936826b2e0f366868
AKusakin <anatolii.kusakin1234@gmail.com>
2019-09-24 16:40:37 +0500

    Table29Commit

--- js/Request.js
+++ js/Request.js
@@ -1,11 +1,19 @@
 let xhr = new XMLHttpRequest();
-
-xhr.open('GET', '/article/xmlhttprequest/example/load');
+xhr.open('GET', 'https://swapi.co/api/people/1/');
 xhr.send();
+xhr.onreadystatechange = function() {
+  if (xhr.readyState == 3) {
+    alert(`Загрузка`);
+  }
+};
 xhr.onload = function() {
   if (xhr.status != 200) {
     alert(`Ошибка ${xhr.status}: ${xhr.statusText}`);
-  } else {
-    alert(`Готово, получили ${xhr.response.length} байт`);
+    return;
   }
+  alert(`Готово, получено ${xhr.response.length} байт`);
+  var map = new Map(JSON.parse(xhr.response));
+  map.get("name");
+  alert(xhr.response.slice(1,7));
 };
+
